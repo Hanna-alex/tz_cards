0227992d56882bcdf5ab60ce126ab3384433af0c
Hanna-alex <64468077+Hanna-alex@users.noreply.github.com>
2022-12-06 14:55:27 +0600

    fix Changing the screen width changes the content

--- js/main.js
+++ js/main.js
@@ -2,141 +2,61 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  let userWidth = document.documentElement.clientWidth;
+  const dbDesc = './db/descktop.json'
+  const dbMobile = './db/mobile.json'
 
+  let userWidth = null;
 
+  updateDimension()
+  sizeСhanges()
 
-  if (userWidth <= 991) {
-
-    //подключение к базе данныx 
-    const getData = () => {
-      return fetch('./db/mobile.json')
-        .then((response) => {
-          if (response.ok) {
-            return response.json()
-          } else {
-            throw new Error('Данные получены с ошибкой')
-          }
-        })
-
-        .then((data) => {
-          // console.log(data);
-          render(data);
-        })
-
-        .catch((error) => {
-          console.error(error.message);
-        })
-    }
-    //
-
-    getData()
-
-    //отображение товара на странице
-    const render = (data) => {
-      const cardContainer = document.querySelector('.excursions__cards');
-
-      cardContainer.innerHTML = '';
-      data.forEach(card => {
-
-        cardContainer.insertAdjacentHTML('beforeend', `
-      <div class="excursions__card card">
-            <div class="card__img">
-              <img
-                src="img/excursions_foto/${card.img}"
-                alt="Картинка: экскурсия"
-              />
-              <div class="card__label ${card.labelClass}">
-                <span>${card.labelText}</span>
-              </div>
-              <!-- /.card__label -->
-            </div>
-            <!-- /.card__img -->
-            <div class="card__info">
-              <div class="card__time time">
-                <svg class="time__icon">
-                  <use xlink:href="img/icons/sprite.svg#oclock"></use>
-                </svg>
-                <span class="time__text">2 часа</span>
-              </div>
-              <!-- /.card__time -->
-              <h3 class="card__title">
-                <span class="action">АКЦИЯ - </span>Обзорная экскурсия
-                по&nbsp;рекам и&nbsp;каналам с&nbsp;остановками Hop on&nbsp;Hop
-                Off 2019
-              </h3>
-              <ul class="card__list list">
-                <li class="list__item">Билет на целый день</li>
-                <li class="list__item">Неограниченное число катаний</li>
-                <li class="list__item">
-                  6 остановок у главных достопримечательностей
-                </li>
-                <li class="list__item">
-                  Ближайший рейс сегодня
-                  <span class="list__time-data time-data">
-                    
-                  </span>
-                </li>
-              </ul>
-              <div class="card__price-more">
-                <div class="card__price price">
-                  <span class="price__sum">900 &#8381;</span>
-                  <span class="price__on-pier">1200 р на причале</span>
-                </div>
-                <!-- /.card__price price -->
-                <div class="card__more more">
-                  <button class="card__btn">Подробнее</button>
-                </div>
-                <!-- /.more -->
-              </div>
-              <!-- /.card__price-more -->
-            </div>
-            <!-- /.card__info -->
-          </div>
-          <!-- /.excursions__card card -->`)
-      })
-
-      getTimeElems(data);
-
-    }
-    ///render
-
-
-
-
-  } else if (userWidth > 991) {
-
-    const getData = () => {
-      return fetch('./db/descktop.json')
-        .then((response) => {
-          if (response.ok) {
-            return response.json()
-          } else {
-            throw new Error('Данные получены с ошибкой')
-          }
-        })
-
-        .then((data) => {
-          render(data);
-          changeTime(data);
-
-        })
-
-        .catch((error) => {
-          console.error(error.message);
-        })
-    }
-
-    getData()
-
-
+  function sizeСhanges() {
+    window.addEventListener('resize', sizeСhanges)
+    updateDimension()
   }
 
-  //////////////////////////////////////////////////////
+
+  function updateDimension() {
+    userWidth = window.innerWidth
+
+    if (userWidth <= 991) {
+      //подключение к базе данныx 
+      getData(dbMobile)
+
+
+    } else {
+
+      //подключение к базе данныx
+      getData(dbDesc)
 
 
 
-  ////////////////////////////////////////////////
+    }
+  }
+
+  function getData(name) {
+
+    return fetch(name)
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('Данные получены с ошибкой')
+        }
+      })
+
+      .then((data) => {
+        render(data);
+        // changeData(data)
+        renderTime(data)
+
+      })
+
+      .catch((error) => {
+        console.error(error.message);
+      })
+  }
+
   const render = (data) => {
     const cardContainer = document.querySelector('.excursions__cards');
 
@@ -206,112 +126,68 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
 
+  //////////////////////////////////////////////////////////////////////////
   let stack = 4;
   let count = 1;
-  let btn
+
+  const renderTime = (data) => {
+    const cards = document.querySelectorAll('.time-data')
+    const newStack = stack * count
+
+    for (let i = 0; i < data.length; i++) {
+
+      cards[i].innerHTML = ' '
 
 
-  const renderTimeElems = (n, timeN, newStack, indexE) => {
-    let timeContainers = document.querySelectorAll('.list__time-data');
 
-    for (let d = 0; d < timeContainers.length; d++) {
+      if (data[i].times.length < 5) {
 
-      if (indexE === d) {
+        data[i].times.forEach(elem => {
 
-        if (timeN.length <= newStack) {
 
-          timeContainers[d].innerHTML = ' ';
+          cards[i].insertAdjacentHTML('beforeend', `
+                  <span class="time-data__time">`+ elem + `</span>`)
 
-          n.forEach(timesElem => {
-            timeContainers[d].insertAdjacentHTML('beforeend', `
-                  <span class="time-data__time">`+ timesElem + `</span>`)
-          })
+        })
 
-        } else {
-          timeContainers[d].innerHTML = ' ';
+      } else {
 
-          n.forEach(timesElem => {
-            timeContainers[d].insertAdjacentHTML('beforeend', `
-                  <span class="time-data__time">`+ timesElem + `</span>`)
-          })
+        sliceArray(data[i].times, newStack).forEach(elem => {
 
-          timeContainers[d].insertAdjacentHTML('afterend', `
+
+          cards[i].insertAdjacentHTML('beforeend', `
+                  <span class="time-data__time">`+ elem + `</span>`)
+
+        })
+        cards[i].insertAdjacentHTML('afterend', `
                   <span class="time-data__btn"> еще...</span>`);
 
-          btn = document.querySelector('.time-data__btn');
-          btn.addEventListener('click', () => {
-
-            ++count;
-            console.log(newStack);
-            getTime()
-            if (timeN.length > newStack) {
-              btn.style.display = "none"
-            }
-
-          });
-
-        }
       }
-    }
-    //////////////////////////
 
+      let btns = document.querySelectorAll('.time-data__btn')
 
-  }
+      console.log(btns);
 
-  const sliseArr = (arr, index) => {
-    return arr.slice(0, index - 1)
-
-  }
-
-  const sliseArrN = (arr, index) => {
-
-    return arr.slice(0, index)
-
-  }
-
-  const changeTime = (data) => {
-
-    const newStack = stack * count;
-    for (let d = 0; d < data.length; d++) {
-      let timeN = data[d].times; // ArrTime *3   
-      let indexE = d;
-
-      if (timeN.length > newStack) {
-        renderTimeElems(sliseArr(timeN, newStack), timeN, newStack, indexE);
-        console.log(newStack);
-      } else {
-        renderTimeElems(sliseArrN(timeN, newStack), timeN, newStack, indexE);
-
-      }
     }
 
-  }
-
-  const getTime = () => {
-    return fetch('./db/descktop.json')
-      .then((response) => {
-        if (response.ok) {
-          return response.json()
-        } else {
-          throw new Error('Данные получены с ошибкой')
-        }
-      })
-
-      .then((data) => {
-        changeTime(data);
-
-      })
-
-      .catch((error) => {
-        console.error(error.message);
-      })
 
   }
 
 
+  function sliceArray(arr, index) {
+    console.log(arr);
+    return arr.slice(0, (index - 1))
+
+  }
+
+  let btns = document.querySelectorAll('.time-data__btn')
+
+  console.log(btns);
 
 
 
+
+  ////// 
 })
 //end
 
